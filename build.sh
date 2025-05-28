7d4cc902dd299ab350b564d01ae7a083f96bb5e9
d06i <llll@DESKTOP-C8VGJLV>
2025-05-28 16:41:28 +0300

    script for linux

---
+++ build.sh
@@ -0,0 +1,24 @@
+echo "Installing Foam Graph..."
+
+echo "Installing frontend (React)..."
+cd frontend || exit
+npm install
+
+if [ -d "build" ]; then
+    echo "old 'build' directory deleting..."
+    rm -rf build
+fi    
+
+npm run build
+if [ -d build]; then
+    echo "Moving 'build' directory to backend..."
+    mv build ../backend/
+fi
+
+cd ..
+
+echo "Installing backend (Actix.rs)..."
+cd backend
+cargo run --release 
+
+read -p "Press enter to continue..."
